6b94b2093ad23bf37f20c194ee43c44c8b32afb3
Zack Stout <zackstout@Zacks-MacBook-Pro.local>
2019-03-31 21:55:34 -0500

    showing paths but lags

--- client.js
+++ client.js
@@ -4,6 +4,7 @@ let numCells = 5;
 let cellWidth;
 let cells = [];
 let angle = 0;
+let counter = 0;
 
 function setup() {
     w = 800;
@@ -42,7 +43,20 @@ function draw() {
         r.circ();
     }
 
-    angle += 0.01;
+    for (let i = 0; i < cells.length; i++) {
+        if (cells[i].x !== 0 && cells[i].y !== 0) {
+            cells[i].shape();
+            cells[i].showPath();
+        }
+    }
+
+    if (angle < 2 * PI) {
+        angle += 0.01;
+
+    } else {
+        angle = 0;
+    }
+    counter ++;
 }
 
 function getCell(x, y) {
@@ -55,6 +69,9 @@ class Cell {
     constructor(x, y) {
         this.x = x;
         this.y = y;
+        this.xPos = 0;
+        this.yPos = 0;
+        this.path = [];
     }
 
     show() {
@@ -72,8 +89,38 @@ class Cell {
         const r = cellWidth * 0.8;
         ellipse(cx, cy, r);
         fill('green');
-        const speed = this.x === 0 ? 2 * this.y : 2  * this.x;
-        ellipse(cx + r * cos(angle * speed - PI / 2) / 2, cy + r * sin(angle * speed - PI / 2) / 2, cellWidth / 12);
+        const speed = this.x === 0 ? 2 * this.y : 2 * this.x;
+        this.xPos = cx + r * cos(angle * speed - PI / 2) / 2;
+        this.yPos = cy + r * sin(angle * speed - PI / 2) / 2;
+        ellipse(this.xPos, this.yPos, cellWidth / 12);
+        pop();
+    }
+
+    shape() {
+        const xPos = getCell(this.x, 0).xPos;
+        const yPos = getCell(0, this.y).yPos;
+
+        push();
+        translate(this.x * cellWidth, this.y * cellWidth);
+        fill('plum');
+        ellipse(xPos, yPos, cellWidth / 12);
+
+        if (counter % 5 === 0) {
+            this.path.push({x: xPos, y: yPos});
+
+        }
+
+        pop();
+    }
+
+    showPath() {
+        push();
+        translate(this.x * cellWidth, this.y * cellWidth);
+        fill('black');
+        this.path.forEach(p => {
+            ellipse(p.x, p.y, 1);
+        });
+
         pop();
     }
 }
